f8faa747e0800d0af3db782935ef1f8ad2331e83
lgarcia99 <54911648+lgarcia99@users.noreply.github.com>
2020-02-11 05:43:24 -0700

    first commit

---
+++ app/src/main/java/garcia/luis/myfirstapplication/MainActivity.kt
@@ -0,0 +1,37 @@
+package garcia.luis.myfirstapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    var counter:Int = 0
+    var num:Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val label:TextView = findViewById(R.id.etiqueta)// as TextView
+        val down:Button = findViewById(R.id.down)// as Button
+        val up:Button = findViewById(R.id.up)
+        val generate:Button = findViewById(R.id.generate)
+
+        generate.setOnClickListener {
+            num = Random.nextInt(0,100)
+
+            label.setText(""+num+"")
+        }
+
+        up.setOnClickListener {
+            // TODO
+        }
+
+        down.setOnClickListener {
+            // TODO
+        }
+    }
+}
